96bf1529858d859b7e32867b4057e2167dd7ffc2
HIROKI <zamadk42@gmail.com>
2021-01-20 13:42:12 +0900

    JavaScript課題②を修正しました。

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click',() => {
+  document.querySelector('button').addEventListener('click',() => { 
+    
+    const list = document.querySelector('ul');
+    while(list.hasChildNodes()) {
+      list.removeChild(list.childNodes[0]);
+    }
+    
     const fizz = document.getElementById('fizz').value;
     const buzz = document.getElementById('buzz').value;
-
+    
     const Fizz = parseFloat(fizz);
     const Buzz = parseFloat(buzz);
     
 
     for(let i = 1; i < 100; i++) {
       const li = document.createElement('li');
-
-      if(Number.isInteger(Fizz) === true && Number.isInteger(Buzz) === true) {
+      
+      if(Number.isInteger(Fizz) && Number.isInteger(Buzz)) {
         if(i % Fizz === 0 && i % Buzz === 0) {
           li.textContent = `FizzBuzz ${i}`;
           document.querySelector('ul').appendChild(li);
